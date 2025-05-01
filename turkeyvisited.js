8a6440da1b7532a70f034f6dd356a15ab735e0b3
Omer Dogan <omerdogan1088@gmail.com>
2025-05-01 15:56:23 +0900

    Updated province colors to match Seyahatya branding

--- turkeyvisited.js
+++ turkeyvisited.js
@@ -1,5 +1,5 @@
-const HOVER_COLOR = "#468FAF";
-const MAP_COLOR = "#EBF2FA";
+const HOVER_COLOR = "#FF8C42";
+const MAP_COLOR = "#FDF6EC";
 let cityCount = localStorage.getItem("selectedCities")
   ? JSON.parse(localStorage.getItem("selectedCities")).length
   : 0;
